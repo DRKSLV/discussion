0d412da1d55ebc19b8b645b423b52fe64985567b
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2020-05-07 10:40:23 +0200

    Deploy lol

--- components/layout/Container.js
+++
@@ -1,9 +0,0 @@
-import style from "../../style/container.module.sass";
-
-export function Container(props) {
-    return (
-        <div className={style.container}>
-            {props.children}
-        </div>
-    );
-}
--- components/threadindent.js
+++
@@ -1,7 +0,0 @@
-import style from "../style/post.module.sass";
-
-export function Threadindent(props) {
-    return (
-        <div className={style.threadindent}></div>
-    );
-}
